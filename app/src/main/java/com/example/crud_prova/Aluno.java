e579a916107c5fab62b2bb5ea725db97a6d2ddb7
Giovanna B. dos Santos <giovanna.berato@gmail.com>
2023-12-06 07:21:23 -0300

    Initial commit

---
+++ app/src/main/java/com/example/crud_prova/Aluno.java
@@ -0,0 +1,47 @@
+package com.example.crud_prova;
+
+import java.io.Serializable;
+
+public class Aluno implements Serializable {
+    private int id;
+    private String nome;
+    private String cpf;
+    private String telefone;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public static String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    @Override
+    public String toString(){
+        return nome;
+    }
+}
